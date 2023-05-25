8ec4e4092a11a8cf0e32909409eed9b68a7f71ab
SDudyuk <sdudyuk.o@gmail.com>
2023-05-25 22:21:33 +0300

    add config and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,31 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { TansactionModule } from './tansaction/tansaction.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, AuthModule, CategoryModule, TansactionModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    CategoryModule,
+    TansactionModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'postgres',
+        host: ConfigService.get('DB_HOST'),
+        port: ConfigService.get('DB_PORT'),
+        username: ConfigService.get('DB_USERNAME'),
+        password: ConfigService.get('DB_PASSWORD'),
+        database: ConfigService.get('DB_NAME'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.js, .ts}'],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
